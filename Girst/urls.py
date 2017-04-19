b654176742c177bde591204d2442de24043bb1ea
heike07 <dbj2008@yeah.net>
2017-04-19 16:46:23 +0800

    创建 template

--- Girst/urls.py
+++ Girst/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-
-import Gitapp.views as bv
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', bv.index),
+    url(r'^Gitapp/', include('Gitapp.urls')),
 ]
--- Gitapp/views.py
+++ Gitapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello heike07.!yep!str~')
+    return render(request, 'index.html')
