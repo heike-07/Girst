d6598d06043dbb99f517ea2c9046655207eb4cfb
heike07 <dbj2008@yeah.net>
2017-04-20 16:18:40 +0800

    连接数据库提取数据库内容构建模型，通过调用类对象提取数据

--- Gitapp/models.py
+++ Gitapp/models.py
@@ -1,3 +1,8 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='Title')
+    content = models.TextField(null=True)
--- Gitapp/views.py
+++ Gitapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'Gitapp/index.html',{'article': article})
