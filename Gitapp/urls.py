18d068d64f9f1de51d62631717d73759492ec06f
heike07 <dbj2008@yeah.net>
2017-05-02 23:06:28 +0800

    20170502

--- Gitapp/urls.py
+++ Gitapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^index/$', views.index),
-    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^(?P<article_id>[0-9]+)/$', views.article_page, name='article_page'),
 ]
